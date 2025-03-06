ec9e591f71ad1eb471d38121766786689334c197
nagyt24d <nagyt.24d@acsjszki.hu>
2025-03-06 08:30:35 +0100

    first try

---
+++ scripts.js
@@ -0,0 +1,28 @@
+
+var quantity prompt('add meg a darabszámot')
+var type = prompt(Add meg a típust)
+
+console.log(quantity)
+console.log(type)
+
+    var price = type 'Student' ? 300 : 350;
+    var discount = quantity > 10 ? 0.9 : 1;
+    
+    var total = quantity * price = discount
+
+ alert(total);
+
+
+
+alert('üzenet');
+
+/*
+   input:
+           hány jegyet kér?
+           Milyen tipusú jegyet kér? (Student / Adult)
+
+           A diák jegy ára: 300ft
+           Felnőtt jegy ára: 350ft
+           Tíznél több jegy vásárlása esetén jár 10% kedvezmény
+
+*/
