68ea74dc5a2431bb7bd9b4141d459cf7b2e1076c
nagyt24d <nagyt.24d@acsjszki.hu>
2025-03-06 12:59:11 +0100

    full

--- scripts.js
+++ scripts.js
@@ -2,19 +2,28 @@
 var quantity prompt('add meg a darabszámot')
 var type = prompt(Add meg a típust)
 
-console.log(quantity)
-console.log(type)
+ar types = (
+   student: 300
+   adult: 350
+   retired: 280 )
 
-    var price = type 'Student' ? 300 : 350;
+
+
+    var price = types ['type']
     var discount = quantity > 10 ? 0.9 : 1;
     
+    var types = (
+      student: 300
+      adult: 350
+      retired: 280
+
     var total = quantity * price = discount
 
- alert(total);
+   
+    ); 
 
 
-
-alert('üzenet');
+alert('Total');
 
 /*
    input:
@@ -23,6 +32,7 @@ alert('üzenet');
 
            A diák jegy ára: 300ft
            Felnőtt jegy ára: 350ft
+           nyugdíjas jegy ára: 280ft
            Tíznél több jegy vásárlása esetén jár 10% kedvezmény
 
 */
